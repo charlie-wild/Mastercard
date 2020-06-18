5ec90d310cad2d44ebac1e8c1e5c157b7692bc41
Charlie Wild <charliewild@gmail.com>
2020-06-18 10:22:09 +0100

    createHeader function

--- LoadRates/createHeader.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/mastercard/oauth1-signer-go/utils"
-
-func createHeader() string {
-	signingKey, err := utils.LoadSigningKey()
-	//test
-}
--- LoadRates/main.go
+++ LoadRates/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "github.com/mastercard/oauth1-signer-go/utils"
+
 func main() {
-	httpRead()
+	createHeader()
 }
+
+
+
+
+func createHeader() string {
+	signingKey, err := utils.LoadSigningKey(
+		"C:\Users\wildc\Documents\mastercardProject\MCD_Sandbox_mastercard-tm-test_API_Keys",
+		"keystorepassword" //will need to import these for prod
+		if err != {
+			log.Fatalln(err)
+		}
+		return signingKey
+	)
+}
+
